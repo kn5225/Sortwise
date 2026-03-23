36b97190f3c237fa0ebe13e5ab1b15508b690912
kn5225 <149865521+kn5225@users.noreply.github.com>
2026-03-23 19:20:27 -0400

    Update fix_model.py

--- fix_model.py
+++ fix_model.py
@@ -11,23 +11,15 @@ def remove_groups(config):
         for item in config:
             remove_groups(item)
 
-with h5py.File('keras_model.h5', 'r') as f_in:
-    # Read and patch the model config
-    model_config = json.loads(f_in.attrs['model_config'])
+with h5py.File('keras_model_original.h5', 'r') as f_in:
+    model_config = json.loads(f_in.attrs['model_config'])  # Read and patch the model config
     remove_groups(model_config)
-    
-    # Copy everything to a new file with the patched config
-    with h5py.File('keras_model_fixed.h5', 'w') as f_out:
-        # Write patched config
-        f_out.attrs['model_config'] = json.dumps(model_config)
-        
-        # Copy all other attributes
-        for key, val in f_in.attrs.items():
+    with h5py.File('keras_model_fixed.h5', 'w') as f_out: # Copy everything to a new file with the patched config
+        f_out.attrs['model_config'] = json.dumps(model_config) # Write patched config
+        for key, val in f_in.attrs.items():        # Copy all other attributes
             if key != 'model_config':
                 f_out.attrs[key] = val
-        
-        # Copy all weight data
-        def copy_group(src, dst):
+        def copy_group(src, dst):  # Copy all weight data
             for key in src.keys():
                 if isinstance(src[key], h5py.Group):
                     grp = dst.require_group(key)
@@ -36,7 +28,6 @@ with h5py.File('keras_model.h5', 'r') as f_in:
                     dst.create_dataset(key, data=src[key][()])
                 for attr_key, attr_val in src[key].attrs.items():
                     dst[key].attrs[attr_key] = attr_val
-        
         copy_group(f_in, f_out)
 
 print("Done! keras_model_fixed.h5 is ready.")
